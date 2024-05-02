8019efeeb13ed50029f5535109aefe29f4031ff4
Dylan Kneidel <dckneidel@ku.edu>
2024-05-02 14:35:24 -0500

    screw 2 space tabs lol

--- server.js
+++ server.js
@@ -6,28 +6,28 @@ let connectedClients = 0; // Variable to keep track of the number of connected c
 
 // Function to hash client IP
 const hashIdentifier = (input) => {
-  return crypto.createHash('sha256').update(input).digest('hex');
+    return crypto.createHash('sha256').update(input).digest('hex');
 };
 
 wss.on('connection', (ws, req) => {
-  // Increment the count on new connection
-  connectedClients++;
+    // Increment the count on new connection
+    connectedClients++;
   
-  // Hash the remote IP address
-  const userIdentifier = hashIdentifier(req.socket.remoteAddress);
+    // Hash the remote IP address
+    const userIdentifier = hashIdentifier(req.socket.remoteAddress);
   
-  console.log(`Client connected with ID: ${userIdentifier}`);
-  console.log(`Number of connected clients: ${connectedClients}`);
-
-  ws.on('message', (message) => {
-    console.log(`Received message from ID ${userIdentifier} => ${message}`);
-    ws.send(`Server response: ${message}`);
-  });
-
-  ws.on('close', () => {
-    // Decrement the count on disconnection
-    connectedClients--;
-    console.log(`Client with ID ${userIdentifier} disconnected`);
+    console.log(`Client connected with ID: ${userIdentifier}`);
     console.log(`Number of connected clients: ${connectedClients}`);
-  });
+
+    ws.on('message', (message) => {
+        console.log(`Received message from ID ${userIdentifier} => ${message}`);
+        ws.send(`Server response: ${message}`);
+    });
+
+    ws.on('close', () => {
+        // Decrement the count on disconnection
+        connectedClients--;
+        console.log(`Client with ID ${userIdentifier} disconnected`);
+        console.log(`Number of connected clients: ${connectedClients}`);
+    });
 });
