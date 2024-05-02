b90bb0b68b2bff9acbbdba58f6f968df28fe9733
Dylan Kneidel <dckneidel@ku.edu>
2024-05-02 15:28:54 -0500

    better activity tracking

--- client.js
+++ client.js
@@ -1,26 +1,61 @@
-const WebSocketClient = require('websocket').client;
+const WebSocketClient = require('websocket').client
+const readline = require('readline')
 
-var client = new WebSocketClient();
+var client = new WebSocketClient()
+var isActive = true // initial active state
 
 client.on('connectFailed', function(error) {
-    console.log('Connect Error: ' + error.toString());
-});
+    console.log('Connect Error:'+ error.toString())
+})
 
 client.on('connect', function(connection) {
-    console.log('Connection established!');
+    console.log('Connection established!')
     
     connection.on('error', function(error) {
-        console.log("Connection error: " + error.toString());
-    });
+        console.log("Connection error: " + error.toString())
+    })
     
     connection.on('close', function() {
-        console.log('Connection closed!');
-    });
+        console.log('Connection closed!')
+    })
     
-    connection.on('message', function(message) {
-        console.log("Current time on server is: '" + message.utf8Data + "'");
-    });
-});
+    // send initial active state
+    connection.send(JSON.stringify({ active: isActive }))
+    
+    // toggle active state on key press using readline
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    })
+    
+    rl.setPrompt('Press Enter to toggle active state... ')
+    rl.prompt()
+    
+    rl.on('line', function(line) {
+        if (line === 'q') { // if user enters 'q'
+            console.log('Exiting...')
+            if (isActive) {
+                isActive = false
+                connection.send(JSON.stringify({ active: isActive }))
+            }
+            connection.close() // close WebSocket connection
+            rl.close() // close readline interface
+            process.exit(0)
+        } else if (line === '') { // space bar
+            isActive =!isActive
+            connection.send(JSON.stringify({ active: isActive }))
+        }
+        rl.prompt()
+    })
+})
 
-client.connect('ws://localhost:8080/');
+// This was needed for ctrl+c for a minute, but I think it just waits a sec to close out.
+// process.on('SIGINT', function() {
+//     console.log('Received SIGINT, closing WebSocket connection...')
+//     client.abort()
+//     setTimeout(function() {
+//         process.exit(0)
+//     }, 100) // give the WebSocket connection some time to close
+// })
 
+client.connect('ws://localhost:8080/')
--- server.js
+++ server.js
@@ -1,34 +1,48 @@
-const WebSocket = require('ws');
-const crypto = require('crypto');
+const WebSocket = require('ws')
+const crypto = require('crypto')
 
-const wss = new WebSocket.Server({ port: 8080 });
-let connectedClients = 0; // Variable to keep track of the number of connected clients
-let activeClients = 0; // Variable to keep track of the active connected clients
+console.log("server.js is running...")
+const wss = new WebSocket.Server({ port: 8080 })
+let connectedClients = 0 // Variable to keep track of the number of connected clients
+let activeClients = 0 // Variable to keep track of the number of active clients
 
 // Function to hash client IP
 const hashIdentifier = (input) => {
-    return crypto.createHash('sha256').update(input).digest('hex');
-};
+    return crypto.createHash('sha256').update(input).digest('hex')
+}
 
 wss.on('connection', (ws, req) => {
     // Increment the count on new connection
-    connectedClients++;
+    connectedClients++
   
     // Hash the remote IP address
-    const userIdentifier = hashIdentifier(req.socket.remoteAddress);
+    const userIdentifier = hashIdentifier(req.socket.remoteAddress)
   
-    console.log(`Client connected with ID: ${userIdentifier}`);
-    console.log(`Number of connected clients: ${connectedClients}`);
-
+    console.log(`Client connected with ID: ${userIdentifier}`)
+    console.log(`Number of connected clients: ${connectedClients}`)
+    console.log(`Number of active clients: ${activeClients}`)
+    
     ws.on('message', (message) => {
-        console.log(`Received message from ID ${userIdentifier} => ${message}`);
-        ws.send(`Server response: ${message}`);
-    });
-
+        const msg = JSON.parse(message)
+        if (msg.active!== undefined) {
+            if (msg.active) {
+                activeClients++
+            } else {
+                activeClients--
+            }
+            console.log(`Client ${userIdentifier} is now ${msg.active? 'active' : 'inactive'}`)
+            console.log(`Number of active clients: ${activeClients}`)
+        } else {
+            console.log(`Received message from ID ${userIdentifier} => ${message}`)
+            ws.send(`Server response: ${message}`)
+        }
+    })
+    
     ws.on('close', () => {
         // Decrement the count on disconnection
-        connectedClients--;
-        console.log(`Client with ID ${userIdentifier} disconnected`);
-        console.log(`Number of connected clients: ${connectedClients}`);
-    });
-});
+        connectedClients--
+        console.log(`Client with ID ${userIdentifier} disconnected`)
+        console.log(`Number of connected clients: ${connectedClients}`)
+        console.log(`Number of active clients: ${activeClients}`)
+    })
+})
